bfe9b11bc1c922bb4a3629cc60868b4e1dea8070
Rhyker Wells <a.rhykerw@gmail.com>
2025-07-09 04:23:06 +0000

    feat: add internal get api method

---
+++ apiMethods.go
@@ -0,0 +1,36 @@
+// Robloxgo - Roblox bindings for Go
+// Available at https://github.com/RhykerWells/robloxgo
+
+// Copyright 2025 Rhyker Wells <a.rhykerw@gmail.com>.  All rights reserved.
+// License can be found in the LICENSE file of the repository.
+
+// Package robloxgo provides Roblox binding for Go
+
+package robloxgo
+
+import (
+	"net/http"
+)
+
+// get is an internal method that makes a GET request to the specified URL
+
+// If the response status code is not 200 (OK/Successful), it 
+// returns a custom error describing the HTTP status code
+func (c *Client) get(methodURL string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, methodURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != ResponseOK.Code {
+		return nil, getFullHttpError(resp.StatusCode)
+	}
+
+	return resp, nil
+}
