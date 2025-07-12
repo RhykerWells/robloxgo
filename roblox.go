39eec78fdd7d013ddd6389ce6e2a1093cf093e39
Rhyker Wells <a.rhykerw@gmail.com>
2025-07-12 10:08:52 +0000

    chore: bump version

--- roblox.go
+++ roblox.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version of RobloxGo. Follows Semantic Versioning. (https://semver.org)
-const Version = "1.0.0-rc.1"
+const Version = "1.0.0-rc.2"
 
 var robloxGoUserAgent = fmt.Sprintf("RobloxGo v%s (+https://github.com/RhykerWells/robloxgo); Go %s", Version, runtime.Version())
 
