8e00caff4b1b09b9f21995048888f118de2febd7
Rhyker Wells <a.rhykerw@gmail.com>
2025-07-12 01:08:17 +0000

    docs: remove unccessary top level package comment

--- endpoints.go
+++ endpoints.go
@@ -1,10 +1,3 @@
-// Robloxgo - Roblox bindings for Go
-// Available at https://github.com/RhykerWells/robloxgo
-//
-// Copyright 2025 Rhyker Wells <a.rhykerw@gmail.com>.  All rights reserved.
-// License can be found in the LICENSE file of the repository.
-//
-// Package robloxgo provides Roblox binding for Go
 package robloxgo
 
 // CloudAPIVersion is the Opencloud API version used for REST and Websocket API.
